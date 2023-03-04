3394b233314d2157606b2cc42f1687ce6d996ddb
haidi20 <nhaidii@yahoo.com>
2023-03-04 08:49:39 +0800

    remove content web

--- web/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const customAxios = axios.create({
-    baseURL: "http://localhost:8080/api/",
-    headers: {
-        "Content-type": "application/json",
-    }
-});
-
-export default customAxios;
